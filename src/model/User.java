a90b9fa2adea667250fe6f1a1d95d9a8c941ebaa
SmZd <tmtrd7@gmail.com>
2018-02-28 16:25:46 +0700

    Khai báo Class User để quản lý tài khoản đăng nhập

---
+++ src/model/User.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author tmtrd
+ */
+public class User {
+private static final Map<String, User> USERS = new HashMap<String, User>();
+
+    public static User of(String id) {
+        User user = USERS.get(id);
+        if (user == null) {
+            user = new User(id);
+            USERS.put(id, user);
+        }
+        return user;
+    }
+
+    private User(String id) {
+        this.id = id;
+    }
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+}
